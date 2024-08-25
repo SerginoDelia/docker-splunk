854b2eb205504790770ef71539eb34764649e818
Sergino Delia <sergino.co@gmail.com>
2024-08-25 19:39:31 -0400

    updated docker build

--- light/build.sh
+++ light/build.sh
@@ -3,7 +3,9 @@ if [ -z $CURRENT ]; then
 	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 fi
 
-docker build --no-cache=true -t splunk/splunklight:7.1.2 .
+# docker build --no-cache=true -t splunk/splunklight:7.1.2 .
+docker buildx build --platform linux/amd64 --no-cache=true -t splunk/splunklight:7.1.2 .
+
 docker tag splunk/splunklight:7.1.2 splunk/splunklight:latest
 docker tag splunk/splunklight:7.1.2 dennybritz/splunklight:latest
 docker tag splunk/splunklight:7.1.2 dennybritz/splunklight:7.1.2
